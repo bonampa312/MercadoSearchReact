34705524f46773d1c5f2a80585df62f774fd8335
IIs-romeroS-Romero <is-romero@localhost.localdomain>
2017-09-27 14:47:30 -0500

    Render every item name

--- src/App.js
+++ src/App.js
@@ -9,26 +9,55 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      itemName: ''
+      itemName: '',
+      itemsData: ['']
     }
     this.getItems = this.getItems.bind(this)
+    this.setParam = this.setParam.bind(this)
   }
 
-  getItems(event){
-      var itemInput = event.target.value
-      this.setState({itemName: itemInput})
+  getItems(props){
+      var itemInput = this.state.itemName
+      var self = this
+      console.log(this.state.itemName);
       axios.get(apiMLUrl+'sites/MCO/search', {
         params: {
           q: itemInput
         }
       })
       .then(function (response) {
-        console.log(response)
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' + response.status);
+		      return;
+        }
+        self.setState({itemsData: response.data})
+        console.log(self.state.itemsData);
       })
       .catch(function (error) {
         console.log(error)
       })
   }
+
+  setParam(event){
+    var itemInput = event.target.value
+    this.setState({itemName: itemInput})
+  }
+
+  itemsList() {
+     var items = this.state.itemsData
+     console.log("@@@",items)
+     if (items.results) {
+       var listItems = items.results.map(function(item) {return <div><a>{item.title}</a><br/></div>})
+       return(
+         <div>{listItems}</div>
+       )
+     } else {
+       return(
+         <div></div>
+       )
+     }
+
+   }
 
   render() {
     return (
@@ -38,14 +67,15 @@ class App extends Component {
           <h1 className="App-title">MercadoSarch Bonampa312</h1>
           <br/>
           <div className="App-input">
-            <input type="text" onChange={this.getItems}></input>
-            <button>Push Me</button>
+            <input type="text" onChange={this.setParam}></input>
+            <button onClick={this.getItems}>Push Me</button>
           </div>
           <br/>
         </header>
-        <p className="App-intro">
+        <div className="App-intro">
           <a>{this.state.itemName}</a>
-        </p>
+          <div>{this.itemsList()}</div>
+        </div>
         <div className="App-footer">
           <a>Powered by: Santiago Romero</a>
         </div>
